67b59afc0f51a1e9c4d14e4dc1465940562d1869
balu <balugowda1999@gmail.com>
2022-02-25 15:44:08 +0530

    objects

---
+++ sandobj.js
@@ -0,0 +1,41 @@
+// //object literals
+// // let user={//this is an object
+// //     name:'balu',//properties
+// //     age:23,//properties
+// //     email:'balu@gmail.com',//properties
+// //     phone:'9449366255',//properties
+// //     hobbies:['playing badminton','cycling','watching movies']//properties
+// // };
+// // console.log(user);
+// // console.log(user.name);//to access proporties we used the dot operator is one property
+// // console.log(user['name']);//another one is using boxes
+// // console.log(user.hobbies[0]);
+// // console.log(user.hobbies);
+
+// // console.log(typeof user);
+
+
+// let user={//this is an object
+//     name:'balu',//properties
+//     age:23,//properties
+//     email:'balu@gmail.com',//properties
+//     phone:'9449366255',//properties
+//     hobbies:['playing badminton','cycling','watching movies'],//properties
+//     login: function(){
+//         console.log('user logged in');
+//     },
+//     logout:function(){
+//         console.log('user logged out');
+//     },
+//     logHobs:function(){
+//         console.log('this user has some hobbies like');
+//         this.hobbies.forEach(hobby=> 
+//             {
+//            console.log(hobby);            
+//         });
+
+//     }
+// };
+// user.login();
+// user.logout();
+// user.logHobs();
