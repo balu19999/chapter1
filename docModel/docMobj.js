89479d7a9bae842765b20834571b56260cb424bb
balu <balugowda1999@gmail.com>
2022-02-28 15:03:03 +0530

    new folder

---
+++ docModel/docMobj.js
@@ -0,0 +1,18 @@
+// const para =document.querySelector('.error');
+// const paraa =document.querySelector('p');
+// console.log(para)
+// console.log(paraa)
+
+// const para=document.querySelector('div.error')
+// const para=document.querySelectorAll('.error')
+// para.forEach(paraa=>{
+// console.log(paraa);
+// });
+
+// console.log(para);
+
+//get an element by Id
+// const title=document.getElementById('page title');
+// console.log(title);
+
+
